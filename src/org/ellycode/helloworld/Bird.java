0ae5a5c62047c299f2146661b4d499214a2a7a88
Ellay <ellyky2121@gmail.com>
2020-10-05 23:37:26 +0800

    Main

--- src/org/ellycode/helloworld/Bird.java
+++ src/org/ellycode/helloworld/Bird.java
@@ -1,6 +1,6 @@
 package org.ellycode.helloworld;
 
-public class Bird extends Animal{
+public class Bird extends Animal {
 
     public Bird(String name, String color, int legs, boolean hasTale) {
         super(name, color, legs, hasTale);
---
+++ src/org/ellycode/helloworld/Main.java
@@ -0,0 +1,11 @@
+package org.ellycode.helloworld;
+
+public class Main {
+    public static void main(String[] args) {
+        Bird pheonix = new Bird("birdyy", "Blue",2, false);
+        System.out.println(pheonix.getName());
+        pheonix.eat("Bird Seed");
+
+
+    }
+}
