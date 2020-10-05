6973eb09d6e6332eb401adcdb0dc0ae22cad2fdc
Ellay <ellyky2121@gmail.com>
2020-10-06 00:47:38 +0800

    last oop

--- src/org/ellycode/helloworld/Bird.java
+++ src/org/ellycode/helloworld/Bird.java
@@ -2,7 +2,27 @@ package org.ellycode.helloworld;
 
 public class Bird extends Animal {
 
-    public Bird(String name, String color, int legs, boolean hasTale) {
+    private int wings;
+
+    public Bird(String name, String color, int legs, boolean hasTale, int wings) {
         super(name, color, legs, hasTale);
+        this.wings = wings;
+    }
+    public void fly(){
+        System.out.println(this.getName() + "is flying");
+
+    }
+    public int getWings() {
+        return wings;
+    }
+
+    public void setWings(int wings) {
+        this.wings = wings;
+    }
+
+    @Override
+    public void eat(String food) {
+        //super.eat(food);
+        System.out.println("Eating finished!");
     }
 }
--- src/org/ellycode/helloworld/Main.java
+++ src/org/ellycode/helloworld/Main.java
@@ -2,10 +2,13 @@ package org.ellycode.helloworld;
 
 public class Main {
     public static void main(String[] args) {
-        Bird pheonix = new Bird("birdyy", "Blue",2, false);
+        Bird pheonix = new Bird("birdyy", "Blue",2, false,2);
         System.out.println(pheonix.getName());
         pheonix.eat("Bird Seed");
 
 
+        System.out.println(pheonix.getWings());
+        pheonix.fly();
     }
+
 }
